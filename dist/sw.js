1800cd5257cd1ff3542411a03407b545851241e3
TobiasViskum <tobias.t.viskum@gmail.com>
2023-12-24 13:09:35 +0100

    first commit

---
+++ dist/sw.js
@@ -0,0 +1,30 @@
+const cacheName = "version-1.1.9";
+const contentToCache = ["/"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(async () => {
+    const cache = await caches.open(cacheName);
+
+    await cache.addAll(contentToCache);
+  });
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      // console.log(e.request.url);
+
+      const cache = await caches.open(cacheName);
+      try {
+        const response = await fetch(e.request);
+        cache.put(e.request, response.clone());
+        return response;
+      } catch (err) {
+        const r = await caches.match(e.request);
+        if (r) {
+          return r;
+        }
+      }
+    })()
+  );
+});
---
+++ server.mjs
@@ -0,0 +1,41 @@
+import express from "express";
+import webPush from "web-push";
+
+const PORT = 3000;
+const app = express();
+
+const publicKey =
+  "BP-eGQbl1IhWyoQbIMvDOQj30_zVT78tD8pcSQ4vQlr6oWYVb8dZ8gQGlorKt40KJ_iKgIC7uJBX9wb3XKSOxn4";
+const privateKey = "DkTg7U5KQMQTata6l8GdIwoBE2qBcdlHhHLZyQcw8Jc";
+
+app.use(express.json());
+app.use(express.static("dist"));
+
+app.post("/sendNotification", (req, res) => {
+  webPush.setVapidDetails("https://localhost:3000", publicKey, privateKey);
+
+  const body = req.body;
+  const subscription = body.subscription;
+  const payload = body.payload;
+  const options = {
+    TTL: 3600,
+  };
+  const delaySec = 10;
+  setTimeout(() => {
+    webPush
+      .sendNotification(subscription, payload, options)
+      .then(() => {
+        res.sendStatus(200);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+      });
+  }, delaySec * 1000);
+});
+
+app.get("/*", function (req, res) {
+  res.redirect("/");
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
